b3c1ec4459d62541ef6702f597fa960abcfb4d99
frankozz <141370321+frankozz@users.noreply.github.com>
2023-12-16 21:39:11 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    tokens: ["MTE2NTE0Nzc0NDkwMjU4MjMxNg.GwhK7K.XNU8X1JiUBntMVnDUS5mfTLCrq40uMixam7HJk","token","token","token"], ///توكنات حسابات الترحيب
+    voice_channel: "1093724939137327125", ///ايدي الروم اللي بدك يثبت فيه
+    text_channel: "1093724939137327125", ///ايدي روم اللي بدك يرحب فيه
+    your_server: "1032565796246917190", /// ايدي السيرفر تبعك
+}
